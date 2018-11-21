e872de48f64b7744631febda63746fc46caf2535
Joe Eli McIlvain <joe.eli.mac@gmail.com>
2018-11-20 21:49:00 -0800

    Add Pegmatite.build_tree.

--- spec/pegmatite_spec.cr
+++ spec/pegmatite_spec.cr
@@ -1,7 +1,8 @@
 require "./spec_helper"
+require "json"
 
 describe Pegmatite do
-  it "tokenizes basic JSON" do
+  it "tokenizes basic JSON and builds a tree of JSON nodes" do
     source = <<-JSON
     {
       "hello": "world",
@@ -16,7 +17,8 @@ describe Pegmatite do
     }
     JSON
     
-    Pegmatite.tokenize(Fixtures::JSON, source).should eq [
+    tokens = Pegmatite.tokenize(Fixtures::JSON, source)
+    tokens.should eq [
       {:object, 0, 182},
         {:pair, 4, 20},
           {:string, 5, 10}, # "hello"
@@ -46,5 +48,44 @@ describe Pegmatite do
           {:string, 163, 171}, # "problems"
           {:array, 174, 176}, # []
     ]
+    
+    builder = ->(token : Pegmatite::Token, children : Array(JSON::Any)) do
+      kind, start, finish = token
+      case kind
+      when :null then JSON::Any.new(nil)
+      when :true then JSON::Any.new(true)
+      when :false then JSON::Any.new(false)
+      when :string then JSON::Any.new(source[start...finish])
+      when :number then JSON::Any.new(source[start...finish].to_i64)
+      when :array then JSON::Any.new(children)
+      when :pair then JSON::Any.new(children)
+      when :object then
+        object = {} of String => JSON::Any
+        children.each do |pair|
+          key, value = pair.as_a
+          object[key.as_s] = value
+        end
+        JSON::Any.new(object)
+      else raise NotImplementedError.new(kind.inspect)
+      end
+    end
+    
+    result = Pegmatite.build_tree(tokens, builder)
+    
+    result.should eq JSON::Any.new({
+      "hello" => JSON::Any.new("world"),
+      "from" => JSON::Any.new({
+        "name" => JSON::Any.new("Pegmatite"),
+        "version" => JSON::Any.new([
+          JSON::Any.new(0_i64),
+          JSON::Any.new(1_i64),
+          JSON::Any.new(0_i64),
+        ]),
+        "nifty" => JSON::Any.new(true),
+        "overcomplicated" => JSON::Any.new(false),
+        "worse-than" => JSON::Any.new(nil),
+        "problems" => JSON::Any.new([] of JSON::Any),
+      } of String => JSON::Any)
+    } of String => JSON::Any)
   end
 end
--- src/pegmatite.cr
+++ src/pegmatite.cr
@@ -28,4 +28,45 @@ module Pegmatite
       [] of Token
     end
   end
+  
+  # Given a flat array of tokens, build a nested tree where each node is built
+  # by the given proc from the "parent" token and zero or more child nodes.
+  #
+  # Traversal is depth-first. That is, each child is built before its parent,
+  # and the resulting node for each child is given to the parent for building.
+  #
+  # Building is delegated to the given proc, which cannot be a block due to
+  # type system and/or syntax limitations in Crystal with `forall` type vars.
+  # The type returned by the given proc must be treated as always the same (X).
+  def self.build_tree(
+    tokens : Array(Token),
+    proc : Proc(Token, Array(X), X)
+  ) : X forall X
+    # Run build_tree_inner with offset zero, discarding the returned offset.
+    offset, result = build_tree_inner(tokens, 0, proc)
+    result
+  end
+  
+  private def self.build_tree_inner(
+    tokens : Array(Token),
+    offset : Int32,
+    proc : Proc(Token, Array(X), X),
+  ) : {Int32, X} forall X
+    # Get the main token, bailing out if initial offset is out of bounds.
+    return {0, proc.call({:_, 0, 0}, [] of X)} if tokens.size <= offset
+    token = tokens.at(offset)
+    
+    # Visit all "children" of the main token, accumulating the results.
+    children = [] of X
+    offset += 1
+    while offset < tokens.size
+      break if tokens.at(offset)[2] > token[2]
+      offset, result = build_tree_inner(tokens, offset, proc)
+      children << result
+      offset += 1
+    end
+    
+    # Yield to the proc to construct the result from the token and children.
+    {offset - 1, proc.call(token, children)}
+  end
 end
