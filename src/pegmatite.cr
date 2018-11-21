5684f34d18693c88979a028577deb270634ccc6c
Joe Eli McIlvain <joe.eli.mac@gmail.com>
2018-11-20 23:00:39 -0800

    Move Pegmatite::Token definition to its own file.

--- src/pegmatite.cr
+++ src/pegmatite.cr
@@ -3,10 +3,6 @@ require "./pegmatite/*"
 module Pegmatite
   VERSION = "0.1.0"
   
-  # A token is a triple containing a name, a start offset, and end offset,
-  # representing a named pattern that was matched within the overall pattern.
-  alias Token = {Symbol, Int32, Int32}
-  
   # Return the array of tokens resulting from executing the given pattern
   # grammar over the given source string, starting from the given offset.
   # Raises a Pattern::MatchError if parsing fails.
