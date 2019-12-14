dfe4cea75cd3f7d5bcdb29fd22e4253bb4526954
Joe Eli McIlvain <joe.eli.mac@gmail.com>
2019-12-13 22:53:13 -0800

    Goodbye, dear whitespace...
    The world may not appreciate you,
    but you'll be forever indented in my heart.

--- src/pegmatite.cr
+++ src/pegmatite.cr
@@ -2,7 +2,7 @@ require "./pegmatite/*"
 
 module Pegmatite
   VERSION = "0.1.0"
-  
+
   # Return the array of tokens resulting from executing the given pattern
   # grammar over the given source string, starting from the given offset.
   # If an IO is passed, print traces of all parsing activity.
@@ -16,7 +16,7 @@ module Pegmatite
     state = Pattern::MatchState.new
     state.trace = true if io
     length, result = pattern.match(source, offset, state)
-    
+
     # If an IO is passed, print traces of all parsing activity.
     if io
       state.traces.each do |trace|
@@ -34,7 +34,7 @@ module Pegmatite
         end
       end
     end
-    
+
     case result
     when Pattern
       raise Pattern::MatchError.new(source, state.highest_fail)
